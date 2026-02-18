d57bfab9ea3420f2da0a6d85c466142256e4de47
Irina Pletneva <irapletneva2005@yandex.ru>
2026-02-18 22:48:46 +0300

    Добавила удаление и копирование

--- index.js
+++ index.js
@@ -32,4 +32,46 @@ function getTasksFromDOM() {
 function saveTasks(tasks) {
 
 }
+// Возвращение списка задач
+function loadTasks() {
+  const tasksFromStorage = localStorage.getItem('tasks');
+
+  if (tasksFromStorage) {
+    return JSON.parse(tasksFromStorage);
+  }
+
+  return items; 
+}
+
+// Создание DOM-элемента задачи
+function createItem(item) {
+  const clone = document
+    .querySelector('#to-do__item-template')
+    .content
+    .querySelector('.to-do__item')
+    .cloneNode(true);
+
+  const textElement = clone.querySelector('.to-do__item-text');
+  const deleteButton = clone.querySelector('.to-do__item-button_type_delete');
+  const duplicateButton = clone.querySelector('.to-do__item-button_type_duplicate');
+  const editButton = clone.querySelector('.to-do__item-button_type_edit');
+
+  textElement.textContent = item;
+    deleteButton.addEventListener('click', () => {
+    clone.remove();
+
+    const items = getTasksFromDOM();
+    saveTasks(items);
+  });
+    duplicateButton.addEventListener('click', () => {
+    const itemName = textElement.textContent;
+    const newItem = createItem(itemName);
+
+    listElement.prepend(newItem);
+
+    const items = getTasksFromDOM();
+    saveTasks(items);
+  });
+
+}
 
