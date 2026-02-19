6b5f253dbeebe9ebdebdd454d4f850b129d20003
Irina Pletneva <irapletneva2005@yandex.ru>
2026-02-19 19:39:43 +0300

    Исправила добавление элемента

--- index.js
+++ index.js
@@ -11,27 +11,7 @@ const listElement = document.querySelector(".to-do__list");
 const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
-function loadTasks() {
 
-}
-
-function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
-
-}
-
-function getTasksFromDOM() {
-
-}
-
-function saveTasks(tasks) {
-
-}
 // Возвращение списка задач
 function loadTasks() {
   const tasksFromStorage = localStorage.getItem('tasks');
